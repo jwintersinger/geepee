2c44c55b545d8f1217161ca6acdde1abe2e8c194
Jeff Wintersinger <jeff.git@wintersinger.com>
2010-03-22 07:35:52 +0000

    Show legend for graph.

--- javascripts/worker.js
+++ javascripts/worker.js
@@ -1,8 +1,15 @@
 // Chrome (unlike Firefox) will only initialize workers if page served over HTTP, not from local
 // filesystem. To start test server: python -m SimpleHTTPServer 9999
 window.addEventListener('load', function() {
-  var worker = new Worker('javascripts/init.js');
+  var colours = {
+    evolved: 'rgb(0,255,0)',
+    target:  'rgb(0,0,255)'
+  };
+  // Make legend keys the same colour as curves on graph.
+  for(legend_key in colours)
+    document.getElementById(legend_key).style.color = colours[legend_key];
 
+  var worker = new Worker('javascripts/init.js');
   worker.onmessage = function(event) {
     var handlers = {
       best_indiv: function(data) {
@@ -11,7 +18,7 @@ window.addEventListener('load', function() {
         var evaluator = new GeePee();
         evaluator.set_constants(data.constants);
         var f = function(x) { return evaluator.evaluate_indiv(data.indiv, x); };
-        grapher.graph_multiple([f, Math.sin], 0, 2*Math.PI, ['rgb(0,255,0)', 'rgb(0,0,255)']);
+        grapher.graph_multiple([f, Math.sin], 0, 2*Math.PI, [colours.evolved, colours.target]);
 
         // Print human-readable representation of evolved function.
         document.getElementById('best_indiv').innerHTML = evaluator.generate_human_readable(data.indiv);
